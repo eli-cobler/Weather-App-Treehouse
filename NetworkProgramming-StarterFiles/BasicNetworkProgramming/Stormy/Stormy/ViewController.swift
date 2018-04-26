3da8922372d8d1f528d938cb17e00b17b51701cf
eli-cobler <ecobler11@gmail.com>
2018-04-26 16:14:53 -0500

    Initial Commit
    Initial Commit

---
+++ NetworkProgramming-StarterFiles/BasicNetworkProgramming/Stormy/Stormy/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Pasan Premaratne on 2/15/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentSummaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
