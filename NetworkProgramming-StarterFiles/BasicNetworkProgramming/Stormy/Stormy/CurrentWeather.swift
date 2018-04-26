0fb1e7ecca5377487c66549ae57992c3e941b7fc
eli-cobler <ecobler11@gmail.com>
2018-04-26 16:48:03 -0500

    Dark Sky Data Points
    Set up struct to hold our data points that will be coming from Dark Sky API

---
+++ NetworkProgramming-StarterFiles/BasicNetworkProgramming/Stormy/Stormy/CurrentWeather.swift
@@ -0,0 +1,65 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Eli Cobler on 4/26/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationfProbability: Double
+    let summary: String
+    let icon: String
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
