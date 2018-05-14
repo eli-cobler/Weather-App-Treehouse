b045d8835dd34b9264a7dcb33e2cdfde5e91b666
Eli Cobler <ecobler11@gmail.com>
2018-05-14 00:11:02 -0500

    Stubbing the Data

--- NetworkProgramming-StarterFiles/BasicNetworkProgramming/Stormy/Stormy/ViewController.swift
+++ NetworkProgramming-StarterFiles/BasicNetworkProgramming/Stormy/Stormy/ViewController.swift
@@ -17,15 +17,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentSummaryLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let currentWeather = CurrentWeather(temperature: 85.0, humidity: 0.8, precipitationfProbability: 0.1, summary: "Hot!", icon: "clear-day")
+        let currentWeatherViewModel = CurrentWeatherViewModel(model: currentWeather)
+        
+        displayWeather(using: currentWeatherViewModel)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func displayWeather(using viewModel: CurrentWeatherViewModel) {
+        currentTemperatureLabel.text = viewModel.temperature
+        currentHumidityLabel.text = viewModel.humidity
+        currentPrecipitationLabel.text = viewModel.precipitationProbability
+        currentSummaryLabel.text = viewModel.summary
+        currentWeatherIcon.image = viewModel.icon
     }
 }
 
